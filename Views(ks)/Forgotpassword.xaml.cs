a7ed090395644c79e886e484413fbca7d1c887eb
karampb06 <270304617@yoobeestudent.ac.nz>
2024-11-21 14:25:12 +1300

    update

---
+++ Views(ks)/Forgotpassword.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace login_and_register_page.Views_ks_
+{
+    /// <summary>
+    /// Interaction logic for Forgotpassword.xaml
+    /// </summary>
+    public partial class Forgotpassword : Window
+    {
+        public Forgotpassword()
+        {
+            InitializeComponent();
+        }
+    }
+}
